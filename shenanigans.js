df3082c3809202c5f6da4b063302c42190bfb3b9
kf9000 <aapo.kois@student.oulu.fi>
2025-02-21 16:09:46 +0200

    Added logic for bundles

--- shenanigans.js
+++ shenanigans.js
@@ -9,8 +9,12 @@ function createDivs(list, type) {
         let elementDiv = document.createElement("div");
         //name
         let name = document.createElement("p");
-        if(type !== "tracks"){
+        if(type !== "tracks" && type !== "bundles"){
+            console.log(items)
             name.textContent = items.type.displayValue +": " + items.name;
+        } 
+        else if(type === "bundles"){
+            name.textContent = "Bundle: " + items.name;
         }
         else{
             name.textContent = "Track: " + items.artist + " - " + items.title;
@@ -24,6 +28,9 @@ function createDivs(list, type) {
         else if(type === "tracks"){
             image.src = items.albumArt;
         }
+        else if(type === "bundles"){
+            image.src = items.image;
+        }
         else{
             image.src = items.images.small;
         }
@@ -32,39 +39,47 @@ function createDivs(list, type) {
         elementDiv.appendChild(name);
 
         elementDiv.className = type;
-        document.querySelector(".names").appendChild(elementDiv)
+        document.querySelector(".names").appendChild(elementDiv);
     });
 }
 
 getData("https://fortnite-api.com/v2/shop", (data) => {
-    const brItems = []
-    const tracks = []
-    const carParts = []
-    const instruments = []
+    const brItems = [];
+    const tracks = [];
+    const carParts = [];
+    const instruments = [];
+    const bundles = [];
+
     data.data.entries.forEach(function(obj) {
-        if(obj.brItems !== undefined){
-            obj.brItems.forEach(function(items) {
-                brItems.push(items)
-            });
-        }
-        else if(obj.tracks !== undefined){
-            obj.tracks.forEach(function(items) {
-                tracks.push(items)
-            });
-        }
-        else if(obj.cars !== undefined){
-            obj.cars.forEach(function(items) {
-                carParts.push(items)
-            });
+        if(obj.bundle === undefined){
+            if(obj.brItems !== undefined){
+                obj.brItems.forEach(function(items) {
+                    brItems.push(items)
+                });       
+            }
+            else if(obj.tracks !== undefined){
+                obj.tracks.forEach(function(items) {
+                    tracks.push(items)
+                });
+            }
+            else if(obj.cars !== undefined){
+                obj.cars.forEach(function(items) {
+                    carParts.push(items)
+                });
+            }
+            else{
+                obj.instruments.forEach(function(items) {
+                    instruments.push(items)
+                });
+            }
         }
         else{
-            obj.instruments.forEach(function(items) {
-                instruments.push(items)
-            });
+            bundles.push(obj.bundle);
         }
     });
     tracks.sort((a, b) => a.artist.localeCompare(b.artist));
     brItems.sort((a,b) => a.type.displayValue.localeCompare(b.type.displayValue));
+    createDivs(bundles, "bundles");
     createDivs(brItems, "brItems");
     createDivs(tracks, "tracks");
     createDivs(carParts, "carParts");
